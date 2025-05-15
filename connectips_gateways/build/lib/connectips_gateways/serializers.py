455c384185a1ef53f36c5f568adbc27e3a6d44d9
Nischal <neeschal101@gmail.com>
2025-05-15 16:36:08 +0545

    change urls

--- connectips_gateways/build/lib/connectips_gateways/serializers.py
+++
@@ -1,8 +0,0 @@
-# serializers.py
-from rest_framework import serializers
-from .models import CipsPayment
-
-class ConnectIpsPaymentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CipsPayment
-        fields = '__all__'  # Includes all fields from the model
